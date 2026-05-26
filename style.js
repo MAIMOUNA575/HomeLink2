1cc78f2e5b13ad2035ad2e5c4175beeb8f9c3c90
coulibaly maimouna <maicoul78@gmail.com>
2026-05-26 13:53:32 +0000

    message

--- style.js
+++ style.js
@@ -1,6 +1,38 @@
-const signe = document.querySelector('#signe')
+// const signe = document.querySelector('#signe')
 
-signe.addEventListener('click', ()=>{
-    console.log('clicked')
-    signe.classList.toggle('hidden')
-})   
+// signe.addEventListener('click', ()=>{
+//     console.log('clicked')
+//     signe.classList.toggle('hidden')
+// })   
+
+
+
+// const burgerBtn = document.querySelector('#signe');
+// const navMenu = document.querySelector('#menu');
+
+
+// burgerBtn.addEventListener('click', () => {
+//     navMenu.classList.toggle('navMenu');
+// });
+
+
+// burgerBtn.addEventListener('click', () => {
+//     navMenu.classList.toggle('hidden');
+//     burgerBtn.classList.toggle('open'); 
+// });
+
+
+
+
+// Sélectionner les éléments
+const burger = document.getElementById("#burger");
+const menu = document.getElementById("#menu");
+
+// Ajouter un événement au clic
+burger.addEventListener("click", () => {
+
+  // Ajouter ou retirer la classe active
+  menu.classList.toggle("active");
+  burger.classList.toggle("open");
+
+});
