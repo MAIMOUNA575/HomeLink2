22b25a736b5b3e5c2544423c8e865975c6e26236
coulibaly maimouna <coulmouna001@gmail.com>
2026-06-01 06:19:51 +0000

    message

--- style.js
+++ style.js
@@ -1,69 +1,15 @@
-// const signe = document.querySelector('#signe')
+const burger = document.getElementById("signe");
+const menu = document.getElementById("classe");
 
-// signe.addEventListener('click', ()=>{
-//     console.log('clicked')
-//     signe.classList.toggle('hidden')
-// })   
-
-
-
-// const burgerBtn = document.querySelector('#signe');
-// const navMenu = document.querySelector('#menu');
-
-
-// burgerBtn.addEventListener('click', () => {
-//     navMenu.classList.toggle('navMenu');
-// });
-
-
-// burgerBtn.addEventListener('click', () => {
-//     navMenu.classList.toggle('hidden');
-//     burgerBtn.classList.toggle('open'); 
-// });
-
-
-
-
-// Sélectionner les éléments
-// const burger = document.getElementById("#burger");
-// const menu = document.getElementById("#menu");
-
-// // Ajouter un événement au clic
-// burger.addEventListener("click", () => {
-
-//   // Ajouter ou retirer la classe active
-//   menu.classList.toggle("active");
-//   burger.classList.toggle("open");
-
-// });
-
-
-
-
-
-// Sélectionner les éléments
-
-const burger = document.getElementById("burger");
-const menu = document.getElementById("menu");
-
-// événement click
 burger.addEventListener("click", () => {
+    menu.classList.toggle("active");
 
-  // ouvrir / fermer menu
-  menu.classList.toggle("active");
-  if(burger == active){
-    burger.style.display = 'flex'
-  }else{
-    burger.style.display = 'none'
-  }
-});
-
-
-croix.addEventListener("click", () => {
-  
-  if(menu == active){
-    menu.style.display = 'block'
-  }else{
-    menu.style.display = 'none'
-  }
+    // Changement de l'icône
+    if(menu.classList.contains("active")){
+        burger.classList.remove("fa-bars");
+        burger.classList.add("fa-xmark");
+    }else{
+        burger.classList.remove("fa-xmark");
+        burger.classList.add("fa-bars");
+    }
 });
